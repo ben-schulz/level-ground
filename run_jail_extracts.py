6e0da51f9b4ac22a74b01ca0b8d432a8312d5513
ben-schulz <benjamin.john.schulz@gmail.com>
2019-08-24 22:09:02 -0500

    feat(extracts): add current detainees.
    also remove the 'Cookie' header
    from requests, as it appears
    to be optional, and as sending
    an incorrect value appears
    to cause an error.

--- run_jail_extracts.py
+++ run_jail_extracts.py
@@ -9,12 +9,13 @@ _today = datetime.datetime.today()
 formatstr = '%Y-%m-%d'
 datestamp = _today.strftime( formatstr )
 
+output_dir = os.path.expanduser( '~/extract_dump' )
+
+
 def seven_hundred_report():
 
     name = '0700_report'
-    url=f'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.R00070s?run=1&outfmt=13&D_DETAIL=1&R001={datestamp}&R001={datestamp}'
-
-    output_dir = os.path.expanduser( '~/extract_dump' )
+    url = f'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.R00070s?run=1&outfmt=13&D_DETAIL=1&R001={datestamp}&R001={datestamp}'
 
     headers = {
 
@@ -24,20 +25,44 @@ def seven_hundred_report():
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate, br',
         'Referer': 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00080s',
+
         'DNT': '1',
         'Connection': 'keep-alive',
-        'Cookie': 'SH01_MPI00070maxrows=120; JSESSIONID=0BA219728D60754CB329B3C0EB563AC7; nmstat=1565577962084',
         'Upgrade-Insecure-Requests': '1',
     }
 
     return PublicUrl( name, url, output_dir,
-               headers=headers,
-               output_converter=excel_to_csv,
-)
+                      headers=headers,
+                      output_converter=excel_to_csv,
+    )
+
+def current_detainees():
+
+    name = 'current_detainees'
+    url = 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.R00250s?run=2'
+
+    headers = {
+        'Host': 'report.boonecountymo.org',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://report.boonecountymo.org/mrcjava/servlet/SH01_MP.I00290s',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
+
+    return PublicUrl( name, url, output_dir,
+                      headers=headers,
+                      output_converter=excel_to_csv,
+    )
+
 
 fetches = [
 
     seven_hundred_report(),
+    current_detainees(),
 ]
 
 async def _run_all():
